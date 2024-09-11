a79d38706e8d3e1a007e3dbb5b22c8d424187746
Ola Krassowska <ola.krassowska@hotmail.com>
2024-09-11 13:15:02 +0200

    moved creating tables and them setup, added commands to create, view and close tickets connected with database data and embeds

--- cogs/tickets.py
+++ cogs/tickets.py
@@ -6,16 +6,126 @@ from discord.ext import commands
 from discord import app_commands
 from discord import Color
 
+from utils.embeds import create_error_embed
+
+import sqlite3
+
+from typing import Optional, Literal
+
 class Tickets(commands.GroupCog, name="tickets"):
     def __init__(self, client):
         self.client = client
         self.status = True
+        
+        self.db_connection = sqlite3.connect('db/mydatabase.db')
+        self.db_cursor = self.db_connection.cursor()
 
-    @app_commands.command(name="create", description="new ticket")
-    async def create(self, interaction: discord.Interaction):
-        embed = discord.Embed(title="new ticket", description="you have created a new ticket", color=Color.teal(),)
-        await interaction.response.send_message(embed=embed)
+    @app_commands.command(name="create", description="Create a new ticket")
+    async def create(self, interaction: discord.Interaction, title: Optional[str] = None, description: Optional[str] = None):
+        try:
+            server_id = interaction.guild.id
+            ticket_id = self._generate_ticket_id()
+            user_id = interaction.user.id
+            creation_date = datetime.datetime.utcnow().isoformat()
+            
+            self.db_cursor.execute("INSERT INTO tickets (server_id, ticket_id, title, description, created_at, assigned_to) VALUES (?, ?, ?, ?, ?, ?)", (server_id, ticket_id, title or "No title", description or "No description", creation_date, user_id))
+            self.db_connection.commit()
+            
+            embed = discord.Embed(title="New Ticket Created", description=f"Your ticket has been created successfully. Ticket ID: {ticket_id}", color=Color.green(),)
+            
+            await interaction.response.send_message(embed=embed)
+        except Exception as e:
+            embed = create_error_embed(f"An error occurred while creating the ticket. {e}")
+            await interaction.response.send_message(embed=embed)
+            
+    @app_commands.command(name="view", description="View your tickets")
+    async def view(self, interaction: discord.Interaction):
+        try:
+            server_id = interaction.guild.id
+            user_id = interaction.user.id
+            
+            self.db_cursor.execute('''
+                SELECT ticket_id, title, description, created_at, status FROM tickets
+                WHERE server_id = ? and assigned_to = ?
+            ''', (server_id, user_id,))
+            tickets = self.db_cursor.fetchall()
 
+            if tickets:
+                embed = discord.Embed(title="Tickets", color=Color.teal())
+                for ticket in tickets:
+                    ticket_id, title, description, created_at, status = ticket
+                    embed.add_field(
+                        name=f"Ticket #{ticket_id}",
+                        value=f"Title: {title}\nDescription: {description}\nCreated on: {created_at}\nStatus: {status}",
+                        inline=False
+                    )
+            else:
+                embed = discord.Embed(
+                    title="No Tickets Found",
+                    description="There are no tickets for this server.",
+                    color=Color.red()
+                )
+
+            await interaction.response.send_message(embed=embed)
+        except Exception as e:
+            embed = create_error_embed(f"An error occurred while viewing tickets. {e}")
+            await interaction.response.send_message(embed=embed)
+
+    @app_commands.command(name="close", description="Close a ticket")
+    async def close(self, interaction: discord.Interaction, ticket_id: int):
+        try:
+            server_id = interaction.guild.id
+            user_id = interaction.user.id
+            
+            self.db_cursor.execute('''
+                                    SELECT assigned_to FROM tickets WHERE server_id = ? AND ticket_id = ?
+                                    ''', (server_id, ticket_id,))
+                                    
+            assigned_to = self.db_cursor.fetchone()[0]
+            
+            if assigned_to != user_id:
+                embed = discord.Embed(
+                    title="Failure",
+                    description="You are not assigned to this ticket.",
+                    color=Color.red()
+                )
+                await interaction.response.send_message(embed=embed)
+                return
+            
+            self.db_cursor.execute('''
+                UPDATE tickets
+                SET status = 'closed'
+                WHERE server_id = ? AND ticket_id = ? and assigned_to = ?
+            ''', (server_id, ticket_id, user_id,))
+            self.db_connection.commit()
+
+            if self.db_cursor.rowcount > 0:
+                embed = discord.Embed(
+                    title="Ticket Closed",
+                    description=f"Ticket #{ticket_id} has been closed.",
+                    color=Color.green()
+                )
+            else:
+                embed = discord.Embed(
+                    title="Failure",
+                    description="This ticket does not exist or cannot be closed.",
+                    color=Color.red()
+                )
+
+            await interaction.response.send_message(embed=embed)
+        except Exception as e:
+            embed = create_error_embed(f"An error occurred while closing the ticket. {e}")
+            await interaction.response.send_message(embed=embed)
+            
+    def _generate_ticket_id(self):
+        try:
+            self.db_cursor.execute('SELECT MAX(ticket_id) FROM tickets')
+            max_id = self.db_cursor.fetchone()[0]
+            return (max_id or 0) + 1
+        except Exception as e:
+            print(f"Error generating ticket ID: {e}")
+            return None
+        
 async def setup(client):
     if Tickets(client).status:
         print(f"[{datetime.datetime.now()}] [\033[1;33mCONSOLE\033[0;0m]: Cog [\033[1;33m{Tickets.__name__}\033[0;0m] loaded : Status [\033[1;32mEnable\033[0;0m]")
---
+++ db/database.py
@@ -0,0 +1,75 @@
+import sqlite3
+import datetime
+
+def create_tables(cursor):
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS config (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            server_id INTEGER NOT NULL UNIQUE,
+            prefix TEXT DEFAULT '!',
+            admin_role_ids JSON,
+            log_channel_id INTEGER,
+            other_settings JSON,
+            created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+            updated_at TEXT DEFAULT CURRENT_TIMESTAMP
+        );
+    ''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS tickets (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            server_id INTEGER NOT NULL,
+            ticket_id INTEGER NOT NULL UNIQUE,
+            title TEXT,
+            description TEXT,
+            created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+            updated_at TEXT DEFAULT CURRENT_TIMESTAMP,
+            status TEXT CHECK(status IN ('open', 'closed', 'in-progress')) DEFAULT 'open',
+            priority TEXT CHECK(priority IN ('low', 'medium', 'high')) DEFAULT 'medium',
+            assigned_to INTEGER,
+            owner INTEGER,
+            comments JSON
+        );
+    ''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS ticket_permissions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            ticket_id INTEGER NOT NULL,
+            user_id INTEGER NOT NULL,
+            role TEXT CHECK(role IN ('admin', 'user')) DEFAULT 'user',
+            FOREIGN KEY (ticket_id) REFERENCES tickets(id),
+            UNIQUE(ticket_id, user_id)
+        );
+    ''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS reacts (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            server_id INTEGER NOT NULL,
+            react_id INTEGER NOT NULL UNIQUE,
+            channel_id INTEGER,
+            message_id INTEGER,
+            react_emoji TEXT,
+            react_type TEXT CHECK(react_type IN ('reaction', 'mention')) DEFAULT 'reaction',
+            description TEXT,
+            created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+            updated_at TEXT DEFAULT CURRENT_TIMESTAMP,
+            UNIQUE(server_id, react_id)
+        );
+    ''')
+
+async def setup_database(client):
+    conn = sqlite3.connect('db/mydatabase.db')
+    cursor = conn.cursor()
+    print(f"[{datetime.datetime.now()}] [\033[1;35mCONSOLE\033[0;0m]: Database [\033[1;35mSQLite\033[0;0m] setup.") 
+
+    try:
+        for guild in client.guilds:
+            create_tables(cursor)
+        print(f"[{datetime.datetime.now()}] [\033[1;35mCONSOLE\033[0;0m]: tables [\033[1;35mSQLite\033[0;0m] created.")
+    except Exception as e:
+        print(f"[{datetime.datetime.now()}] [\033[91mERROR\033[0;0m]: {e}")
+
+    conn.commit()
+    conn.close()
